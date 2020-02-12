b70e81260701fbd04f6c1cb79590130cde2f53c5
Kip Madden <kipmadden@cosapient.com>
2020-02-12 14:12:48 -0600

    Created PostGreSQL database and loaded data

---
+++ QuickDBD-EmployeeSQL.sql
@@ -0,0 +1,56 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/8hxFyS
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "departments" (
+    "dept_no" char(4)   NOT NULL PRIMARY KEY,
+    "dept_name" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "employees" (
+    "emp_no" INTEGER   NOT NULL PRIMARY KEY,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "gender" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    PRIMARY KEY ("emp_no","salary"),
+    FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no")
+);
+
+CREATE TABLE "titles" (
+    "emp_no" INTEGER   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    PRIMARY KEY ("emp_no","title","from_date"),
+    FOREIGN KEY("emp_no") REFERENCES "employees" ("emp_no")
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" CHAR(4)   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_data" DATE   NOT NULL,
+    PRIMARY KEY ("emp_no","dept_no"),
+    FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no")
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" CHAR(4)   NOT NULL,
+    "emp_no" INTEGER   NOT NULL,
+    "from_date" DATE   NOT NULL,
+    "to_date" DATE   NOT NULL,
+    PRIMARY KEY ("dept_no","emp_no"),
+    FOREIGN KEY ("dept_no") REFERENCES "departments" ("dept_no"),
+    FOREIGN KEY ("emp_no") REFERENCES "employees" ("emp_no")
+);
+
